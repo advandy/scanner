dcf3a904ba698ea5aa2ff6d1b9436169f9c98258
yunhan cheng <yunhan.cheng@sap.com>
2017-03-09 14:12:15 +0100

    Take and crop image

--- app/src/main/java/com/example/d060753/scanner/MainActivity.java
+++ app/src/main/java/com/example/d060753/scanner/MainActivity.java
@@ -1,13 +1,129 @@
 package com.example.d060753.scanner;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    final int PIC_CROP = 2;
+    private static final int ACTION_TAKE_PHOTO_B = 1;
+    private Uri picUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button picBtn = (Button) findViewById(R.id.takePicBtn);
+        setBtnListenerOrDisable(picBtn, mTakePicOnClickListener, MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Button okBtn = (Button) findViewById(R.id.button3);
+        okBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImageView mImageView = (CropImageView) findViewById(R.id.cropImageView);
+                Bitmap copped = mImageView.getCroppedImage();
+                mImageView.setImageBitmap(copped);
+            }
+        });
+    }
+
+    Button.OnClickListener mTakePicOnClickListener = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+        }
+    };
+
+    private void performCrop(){
+        try {
+            //call the standard crop action intent (the user device may not support it)
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            //indicate image type and Uri
+            cropIntent.setDataAndType(picUri, "image/*");
+            //set crop properties
+            cropIntent.putExtra("crop", "true");
+            //indicate aspect of desired crop
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            //indicate output X and Y
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            //retrieve data on return
+            cropIntent.putExtra("return-data", true);
+            //start the activity - we handle returning in onActivityResult
+
+            startActivityForResult(cropIntent, PIC_CROP);
+
+        }
+        catch(ActivityNotFoundException anfe){
+            //display an error message
+            String errorMessage = "Whoops - your device doesn't support the crop action!";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private void dispatchTakePictureIntent(int actionTakePhotoB) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == ACTION_TAKE_PHOTO_B && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            CropImageView mImageView = (CropImageView) findViewById(R.id.cropImageView);
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void saveCrop() {
+        CropImageView mImageView = (CropImageView) findViewById(R.id.cropImageView);
+        Bitmap copped = mImageView.getCroppedImage();
+        mImageView.setImageBitmap(copped);
+    }
+
+    private void setBtnListenerOrDisable(
+            Button btn,
+            Button.OnClickListener onClickListener,
+            String intentName
+    ) {
+        if (isIntentAvailable(this, intentName)) {
+            btn.setOnClickListener(onClickListener);
+        } else {
+            btn.setText("Not Available");
+            btn.setClickable(false);
+        }
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
     }
 }
